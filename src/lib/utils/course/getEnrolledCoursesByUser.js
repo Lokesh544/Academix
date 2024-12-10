1646dffd92a45abe4935072c7f7e319f6224ffe3
jinnykapur <jinnykapur@gmail.com>
2024-12-10 22:10:17 +0530

    Created Enrolled Courses Page

--- src/lib/utils/course/getEnrolledCoursesByUser.js
+++ src/lib/utils/course/getEnrolledCoursesByUser.js
@@ -2,12 +2,14 @@
  * @returns Enroll List of Courses enrolled by User
  *
  * @param {String} userId User to Check
+ * @param {String} search Search Query
  */
-export default async function getEnrolledCoursesByUser(userId) {
+export default async function getEnrolledCoursesByUser(userId, search) {
   const res = await fetch("/api/enrollList/fromUser", {
     method: "POST",
     body: JSON.stringify({
       userId,
+      search,
     }),
   })
     .then((res) => res.json())
--- src/views/dashboard/EnrolledCourses.jsx
+++ src/views/dashboard/EnrolledCourses.jsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import Section from "@/components/wrappers/Section";
-import Link from "next/link";
-import CardRender from "@/components/utils/CardRender";
 import CourseCard from "@/components/basic/CourseCard";
-import { useEffect, useState } from "react";
-import getUserFromId from "@/lib/utils/user/getUserFromId";
-import getEnrolledUsersInCourse from "@/lib/utils/course/getEnrolledUsersInCourse";
+import { TypographyH1 } from "@/components/ui/typography";
+import CardRender from "@/components/utils/CardRender";
+import SearchBar from "@/components/utils/SearchBar";
 import getEnrolledCoursesByUser from "@/lib/utils/course/getEnrolledCoursesByUser";
+import getEnrolledUsersInCourse from "@/lib/utils/course/getEnrolledUsersInCourse";
+import getUserFromId from "@/lib/utils/user/getUserFromId";
 import getUserId from "@/lib/utils/user/getUserId";
 import { localdata } from "@/localdata";
+import { useEffect, useState } from "react";
 
-export default function EnrolledCourses() {
+export default function EnrolledCourses({ search }) {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -23,7 +22,7 @@ export default function EnrolledCourses() {
           localdata.username(),
           localdata.password()
         );
-        getEnrolledCoursesByUser(userId)
+        getEnrolledCoursesByUser(userId, search)
           .then(async (courses) => {
             for (let i in courses) {
               courses[i].data = JSON.parse(courses[i].data);
@@ -47,21 +46,17 @@ export default function EnrolledCourses() {
   }, []);
 
   return (
-    <Section>
-      <div className="flex justify-between">
-        <h3 className="text-xl md:text-4xl lg:text-5xl font-bold inline-block">
-          Enrolled
-          <span className="text-secondary ml-4">Courses</span>
-        </h3>
-        <Button asChild className="bg-secondary hover:bg-secondary/90">
-          <Link href="/dashboard/enrolledCourses">Explore All</Link>
-        </Button>
-      </div>
+    <div className="min-h-[calc(100vh-20rem)]">
+      <TypographyH1 className="capitalize text-center my-8">
+        Enrolled Courses
+      </TypographyH1>
+      <SearchBar action="/dashboard/enrolledCourses" defaultValue={search} />
       <CardRender
         data={courses}
         ItemRender={CourseCard}
+        className="my-16"
         placeholder={loading ? "Loading..." : "No Course Found"}
       />
-    </Section>
+    </div>
   );
 }
