816344f1f0476cba32b7921d9dc18893dc709c38
lokesh <lokesh544.be22@chitkara.edu.in>
2024-12-10 22:31:15 +0530

    Fix

--- src/components/Wrappers/Section.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export default function Section({ className, children, ...props }) {
-  return (
-    <section className={cn("my-16", className)} {...props}>
-      {children}
-    </section>
-  );
-}
--- src/components/utils/StarIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { Star } from "lucide-react";
-/**
- *
- * @param {{glow: boolean}} props
- * @returns
- */
-export default function StarIcon({ glow, ...props }) {
-  return <Star {...(glow && { fill: "#ff0" })} {...props} />;
-}
